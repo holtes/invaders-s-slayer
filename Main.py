7f66552e61d3bf331dbccf129ad9e020c961f488
holtes <holtes10@mail.ru>
2021-01-03 19:02:41 +0300

    Clear code

--- Main.py
+++ Main.py
@@ -231,18 +231,114 @@ def play_function():
         visible_objects.draw(screen)
         xol = counter.render(f"Всего убито: {counter_kill}", True, [0, 0, 0])
         screen.blit(xol, (0, 0))
-        xol = hp_indicator.render(f"Hp: {person.get_hp()}", True, [255, 0, 255])
-        screen.blit(xol, (1700, 1000))
+        hp_ind = hp_indicator.render(f"Hp: {person.get_hp()}", True, [255, 0, 255])
+        screen.blit(hp_ind, (1700, 1000))
         bul = bullet_reload_indicator.render(f"Bullet: {60 - person.count}", True, [255, 0, 255])
         screen.blit(bul, (1700, 70))
         clock.tick(FPS)
         pygame.display.flip()
 
     pygame.display.flip()
-
     main()
     pygame.mixer.music.play(True)
     return
+
+def set_difficulty(value, difficulty):
+    DIFFICULTY = difficulty
+
+def start_the_game():
+    pass
+
+
+surface = None
+main_menu = None
+
+
+def main_background():
+    surface.fill((40, 40, 40))
+
+
+def main(test=False):
+    global main_menu
+    global surface
+
+    # Init pygame
+    pygame.init()
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+    # Create pygame screen and objects
+    surface = pygame.display.set_mode(WINDOW_SIZE)
+    pygame.display.set_caption('Example - Multi Input')
+    clock = pygame.time.Clock()
+
+    # Create menus: Settings
+    settings_menu_theme = pygame_menu.themes.THEME_ORANGE.copy()
+    settings_menu_theme.title_offset = (5, -2)
+    settings_menu_theme.widget_alignment = pygame_menu.locals.ALIGN_LEFT
+    settings_menu_theme.widget_font = pygame_menu.font.FONT_OPEN_SANS_LIGHT
+    settings_menu_theme.widget_font_size = 20
+
+    settings_menu = pygame_menu.Menu(
+        height=WINDOW_SIZE[1] * 0.85,
+        onclose=pygame_menu.events.DISABLE_CLOSE,
+        theme=settings_menu_theme,
+        title='Settings',
+        width=WINDOW_SIZE[0] * 0.9,
+    )
+    settings_menu.add_label("Правила:", label_id="rules")
+    settings_menu.add_label("Перемещение: WASD")
+    settings_menu.add_label("Стрельба: Левая кнопка мыши")
+    settings_menu.add_label("Пауза: Esc")
+    # Add text inputs with different configurations
+
+    # Create selector with 3 difficulty options
+    settings_menu.add_selector('Select difficulty ',
+                               [('Easy', 'EASY'),
+                                ('Medium', 'MEDIUM'),
+                                ('Hard', 'HARD')],
+                               selector_id='difficulty',
+                               default=1)
+
+    settings_menu.add_button('Return to main menu', pygame_menu.events.BACK,
+                             align=pygame_menu.locals.ALIGN_CENTER)
+
+    # Create menus: Main menu
+    main_menu_theme = pygame_menu.themes.THEME_ORANGE.copy()
+    main_menu_theme.widget_offset = (0, 0.09)
+    main_menu_theme.title_font = pygame_menu.font.FONT_COMIC_NEUE
+    main_menu_theme.widget_font = pygame_menu.font.FONT_COMIC_NEUE
+    main_menu_theme.widget_font_size = 30
+
+    main_menu = pygame_menu.Menu(
+        height=WINDOW_SIZE[1],
+        width=WINDOW_SIZE[0],
+        onclose=pygame_menu.events.EXIT,  # User press ESC button
+        title='Main menu',
+        theme=main_menu_theme,
+    )
+
+    main_menu.add_button('Play', play_function, font_size=100)
+    main_menu.add_button('Settings', settings_menu)
+    main_menu.add_button('Quit', pygame_menu.events.EXIT)
+    assert main_menu.get_widget('last_name') is None
+
+    # Main loop
+    while True:
+        # Tick
+        clock.tick(FPS)
+
+        # Paint background
+        main_background()
+
+        # Main menu
+        main_menu.mainloop(surface, main_background, disable_loop=test, fps_limit=FPS)
+
+        # Flip surface
+        pygame.display.flip()
+
+        # At first loop returns
+        if test:
+            break
 
 
 if __name__ == '__main__':
